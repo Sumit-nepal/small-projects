eb99e8d69c8484497f79ac0261d67fbf33341bb2
Sumit Nepal <127305760+Sumit-nepal@users.noreply.github.com>
2023-06-28 19:23:54 +0545

    js completed

--- Rock Paper Scissor/script.js
+++ Rock Paper Scissor/script.js
@@ -1,48 +1,64 @@
-let winMsg = "Victory";
-let loseMsg = "Crushing Defeat";
-let tieMsg = "Tie";
-let moveList = ["Rock","Paper","Scissors"];
-let statusDisplay = document.querySelector("#status-head");
-let buttons = document.querySelectorAll("button");
+let moveList = ["Rock", "Paper","Scissor"];
+function startGame(){
+    let statusDisplay = document.querySelector("#status-head");
+    let buttons = document.querySelectorAll("button");
+    let moveDisplays = document.querySelectorAll(".move-display h2");
+    statusDisplay.textContent = 'Choose!!';
+    for(i = 0; i < buttons.length; i++){
+        buttons[i].textContent = moveList[i];
+    }
+    
+    let r = document.querySelector("#rock-button");
+    r.addEventListener("click",rok);
 
+    function rok(){
+        player = moveList[0];
+        computer = moveList[randomMove()];
+        moveDisplays[0].textContent = "You Played "+player;
+        moveDisplays[1].textContent = "Computer played "+computer;
+        statusDisplay.textContent = calcResult(player,computer);
+    }
+    let p = document.querySelector("#paper-button");
+    p.addEventListener("click",pap);
 
-/**
- * Computes result of the game. returns victory message if move 1 wins.
- * @param  {Number}   move1  move 1
- * @param  {Number}   move2  move 2
- * @return {String}   result result of the game
- */
+    function pap(){
+        player = moveList[1];
+        computer = moveList[randomMove()];
+        moveDisplays[0].textContent = "You Played "+player;
+        moveDisplays[1].textContent = "Computer played "+computer;
+        statusDisplay.textContent = calcResult(player,computer);
+    }
+    let s = document.querySelector("#scissors-button");
+    s.addEventListener("click",sci);
 
-function calcResult(move1, move2) {}
-
-/**
- * @return {Number}   random number between 0 and 2
- */
-
-function randomMove() {
-  return Math.floor(Math.random() * 3);
+    function sci(){
+        player = moveList[2];
+        computer = moveList[randomMove()];
+        moveDisplays[0].textContent = "You Played "+player;
+        moveDisplays[1].textContent = "Computer played "+computer;
+        statusDisplay.textContent = calcResult(player,computer);
+    }
 }
-
-/**
- * Displays start state of game
- */
-
-function startGame() {
-  statusDisplay.textContent = "Choose!";
-
-  // displays the text in the buttons contained in array moveList
-  for (let i = 0; i < buttons.length; i ++) {
-    buttons[i].textContent = moveList[i];
-    buttons[i].style.display = "inline-block";
-  }
-}
-
-
-/**
- * Displays end state of game
- * @param {Event} event event containing information of users input.
- */
-
-function endGame(event) {}
-
 startGame();
+
+function calcResult(move1, move2){
+    if (move1==move2){
+        return "Tie"
+    }
+    if (move1==moveList[0] && move2 ==moveList[2]){
+        return " You Win"
+    }
+    if (move1==moveList[1] && move2 ==moveList[0]){
+        return " You Win"
+    }
+    if (move1==moveList[2] && move2 ==moveList[1]){
+        return "You Win"
+    }
+    else{
+        return "You Loose"
+    }
+}
+function randomMove(){
+    let random = Math.floor(Math.random() * 3);
+    return random;
+}
